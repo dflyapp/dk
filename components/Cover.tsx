ba7727d7437502cf8da69e07c041bbe966fee50f
duongital <duongital@gmail.com>
2024-09-13 09:55:16 +0700

    chore: fix responsive

--- components/Cover.tsx
+++ components/Cover.tsx
@@ -6,7 +6,7 @@ import FlowerRight from 'assets/floral-right.png'
 function Cover() {
   const SIZE = 200
   return (
-    <div className="flex justify-between items-center my-12">
+    <div className="flex justify-between items-center my-12 flex-col md:flex-row">
       <Image placeholder="blur" width={SIZE} height={SIZE} alt="left" src={FlowerLeft} />
       <div className="flex flex-col text-center whitespace-nowrap text-secondary font-hand">
         <h1 className="text-3xl md:text-6xl">Save the date</h1>
